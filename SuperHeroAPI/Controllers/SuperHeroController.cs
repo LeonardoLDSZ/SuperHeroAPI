37bdc91d6d5e7c5e8e01f457892fffae1c9cb5cf
LeonardoLDSZ <leonardodavidesouza@gmail.com>
2022-06-20 21:14:22 -0300

    Some fix

--- SuperHeroAPI/Controllers/SuperHeroController.cs
+++ SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -37,7 +37,7 @@ namespace SuperHeroAPI.Controllers
             var hero = heroes.Find(h => h.Id == id);
             if (hero == null)
                 return BadRequest("Hero not found!");
-            return Ok(heroes);
+            return Ok(hero);
         }
 
         [HttpPost]
@@ -46,5 +46,32 @@ namespace SuperHeroAPI.Controllers
             heroes.Add(hero);
             return Ok(heroes);
         }
+
+        [HttpPut]
+        public async Task<ActionResult<List<SuperHero>>> UpdateHero (SuperHero request)
+        {
+            var hero = heroes.Find(h => h.Id == request.Id);
+            if (hero == null)
+                return BadRequest("Hero not found!");
+
+            hero.Name = request.Name;
+            hero.FirstName = request.FirstName;
+            hero.Lastname = request.Lastname;
+            hero.Place = request.Place;
+            return Ok(heroes);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<SuperHero>>> DeleteHero(int id)
+        {
+            var hero = heroes.Find(h => h.Id == id);
+            if (hero == null)
+                return BadRequest("Hero not found!");
+
+            heroes.Remove(hero);
+            return Ok(heroes);
+        }
+
+
     }
 }           
